40b2a32d1aeafd997b1cf8882a15f2ded4d6d68f
frupreneur <frupreneur@gmail.com>
2023-04-18 15:14:57 +0100

    Analytics Added

--- components/Layout.js
+++ components/Layout.js
@@ -3,6 +3,8 @@ import Head from "next/head";
 import { Header, Footer, Meta } from "../components";
 import { Andika } from "@next/font/google";
 import Script from "next/script";
+import * as gtag from "@/utils/gtag";
+import { useRouter } from "next/router";
 
 const andika = Andika({
   subsets: ["latin"],
@@ -11,9 +13,37 @@ const andika = Andika({
 });
 
 export default function Layout({ children }) {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageView(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
-      
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Head>
         <link rel="shortcut icon" href="/assets/favicon.ico" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,16 @@ export default function Document() {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7721600787257010"
           crossorigin="anonymous"
         ></script>
+
+        {/* <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-147964524-28"
+        ></Script>
+        <Script id="GAcode">
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag(`js`, new Date()); gtag(`config`, `UA-147964524-28`);
+        </Script> */}
       </Head>
       <body>
         <Main />
